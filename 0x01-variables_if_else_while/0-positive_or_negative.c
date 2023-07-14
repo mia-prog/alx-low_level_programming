dbd9ae3df26ec762605e65a46dee217966413eae
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 13:34:14 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	char a = "the number is positive";
+	char* a = "the number is positive";
 	if (n>0){
 		return (a);
 	}
