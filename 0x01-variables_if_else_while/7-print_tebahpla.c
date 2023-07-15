d39db4d20d0c89a53b90528e8c2d1c3460cd3bb6
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 19:38:09 -0700

    task6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+        char alphabet;
+
+        for (alphabet = 'z'; alphabet <= 'a' ; alphabet--)
+        {
+                putchar(alphabet);
+        }
+        putchar('\n');
+        return (0);
+        
+}
