7bb6d5b76e286bdfd47601d6f21540ee3de8909f
mia-prog <mechtoubmeriem@gmail.com>
2023-07-15 05:44:11 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	putchar("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
