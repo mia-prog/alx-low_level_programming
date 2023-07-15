7b32573199a75e5d28379f641017c2e3d1eca2b5
mia-prog <mechtoubmeriem@gmail.com>
2023-07-15 07:52:45 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - prints numbers of base 16 in lowercase
+  *Return: 0
+*/
+int main(void)
+{
+	printf("Size of a char:\n", sizeof(charType));
+	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n");
+	printf("Size of a float: %zu byte(s)\n");
+        printf("\n");
+        return (0);
+}
