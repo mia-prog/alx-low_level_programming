9108936bc70c925f659b21bfc687e5f77d57a25f
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 17:54:11 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+        char alphabet;
+
+        while(alphabet != q) && (alphabet != e)
+        {
+                putchar(alphabet);
+		alphabet++;
+        }
+        putchar('\n');
+        return (0);
+}
