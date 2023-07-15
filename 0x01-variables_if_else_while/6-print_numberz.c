69bb579e1e1ab2ca1a8c612aa5be2e322b2ec8be
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 19:30:44 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -16,10 +16,10 @@ int main(void)
 
 	int n;
 
-        for ( n = '0' ; n <= '9' ; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-        return (0);
+	for ( n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
