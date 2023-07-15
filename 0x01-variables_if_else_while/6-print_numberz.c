5282711e5ae28657ee088ade95db27e744675043
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 18:43:55 -0700

    task5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+        int n;
+
+        for (n = 0 ; n < 10 ; n++)
+        {
+                putchar("%d", n);
+        }
+        putchar('\n');
+        return (0);
+}
