19c14dc3e1309b1e81283f9aa0471095ffa99811
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 17:42:49 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,11 +16,12 @@ int main(void)
         for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
         {
                 putchar(alphabet);
-		for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
-		{
-			putchar(alphabet);
-		}
-        }
-        putchar('\n');
+        }  
+	for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
         return (0);
 }
