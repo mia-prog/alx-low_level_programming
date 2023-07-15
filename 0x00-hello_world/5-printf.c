56fe2ea970a42835c11e870c3358ecae0bd5da76
mia-prog <mechtoubmeriem@gmail.com>
2023-07-15 05:57:51 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - prints numbers of base 16 in lowercase
+  *Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
