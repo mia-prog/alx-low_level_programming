aeb741207792fdd125b03c0958a14a43a87335d1
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 17:23:50 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,12 +2,11 @@
 
 int main(void)
 {
-    char alphabet;
-
-    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-    {
-        putchar(alphabet);
-    }
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
 
     putchar('\n');
     return (0);
