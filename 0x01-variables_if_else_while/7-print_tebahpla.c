deb16e4309da21cddd8c6cb2697430b459719ef8
mia-prog <mechtoubmeriem@gmail.com>
2023-07-14 19:45:07 -0700

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,13 +11,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-        char alphabet;
+	char alphabet;
 
-        for (alphabet = 'z'; alphabet >= 'a' ; alphabet--)
+	for (alphabet = 'z'; alphabet >= 'a' ; alphabet--)
 	{
 		putchar(alphabet);
 	}
 	putchar('\n');
 	return (0);
-        
 }
