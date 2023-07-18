a0d4547cb7e12a3edad827cc021f5f209514590e
mia-prog <mechtoubmeriem@gmail.com>
2023-07-18 09:46:19 -0700

    task1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -prints alphabet in lower_case followed by new line  
+ *return always 0
+ */
+
+void print_alphabet(void)
+{
+        char alph;
+
+	for(i = 0; i<10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+	       	_putchar('\n');
+	}
+}
