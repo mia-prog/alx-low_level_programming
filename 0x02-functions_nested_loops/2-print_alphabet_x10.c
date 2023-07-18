35be19f61d6a0e34518195d4d8a7ff6f2b93642f
mia-prog <mechtoubmeriem@gmail.com>
2023-07-18 10:03:35 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - check main 
+ *print_alphabet_x10 - check main
  *Description: prints alphabet in lower_case followed by new line 10 time
  *return nothing
  */
