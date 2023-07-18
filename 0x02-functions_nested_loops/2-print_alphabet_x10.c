a546485922903a2c020157d5f43cd7cca7421e84
mia-prog <mechtoubmeriem@gmail.com>
2023-07-18 09:59:21 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet -prints alphabet in lower_case followed by new line  
+ *print_alphabet_x10 -prints alphabet in lower_case followed by new line 10 time 
  *return always 0
  */
 
 void print_alphabet_x10(void)
 {
-        int i;
+	int i;
 	char alph;
 
-	for(i = 0; i<10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
 			_putchar(alph);
-	       	_putchar('\n');
+		_putchar('\n');
 	}
 }
