ceab341dd40c52e1e884603a4d555d57cc6bd39f
mia-prog <mechtoubmeriem@gmail.com>
2023-07-19 10:34:45 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,15 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-    
-    if (a > b)
-	    largest = a;
-    else
-        largest = b;
-    if (c > largest)
-    {
-	    largest = c;
-    }
-    
-    return (largest);
+	int largest;
+	
+	if (a > b)
+		largest = a;
+	else
+		largest = b;
+	if (c > largest)
+	{
+		largest = c;
+	}
+	return (largest);
 }
