3c417cd1902dd14fd079f0c8974311337880853a
mia-prog <mechtoubmeriem@gmail.com>
2023-07-24 08:53:17 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - change the value of n to 98 
- *
- * @n : pointer to int 
+ * reset_to_98 - change the value of n to 98
+ * @n : pointer to int
  */
 
 void reset_to_98(int *n)
