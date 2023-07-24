e6d1e937dfb99476589b23cc72ced115e3aa8549
mia-prog <mechtoubmeriem@gmail.com>
2023-07-24 12:06:08 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,13 @@
  * _puts : prints a string
  * @str : pointer to string
  * Return : void
- */ 
+ */
 
 void _puts(char *str)
-{       
+{
 	int i = 0;
-        
-        while (str[i] != 0)
+	
+	while (str[i] != 0)
 	{
 		_putchar(str[i]);
 		i++;
