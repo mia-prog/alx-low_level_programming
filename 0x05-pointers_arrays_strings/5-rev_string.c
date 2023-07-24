42fa540c37ade45d2bcd18f0e10472354fbff6d6
mia-prog <mechtoubmeriem@gmail.com>
2023-07-24 13:50:01 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,24 @@
 void rev_string(char *s)
 {
 	int count = 0;
+	int begin = 0;
 
 	while (s[count] != '\0')
-	{
 		count++;
+	
+
+	for (begin = count - 1; begin >= 0; begin++)
+	{
+		_putchar(s[begin]);
+		begin--;
 	}
 
-	while (count >= 0)
+
+
+
+	/*while (count >= 0)
 	{
 		_putchar(s[count]);
 		count--;
-	}
+	}*/
 }
