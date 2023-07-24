4d9abed8f6f5115bfcafb8c5224cc3514dbc570b
mia-prog <mechtoubmeriem@gmail.com>
2023-07-24 09:00:58 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *
+ * @a : ponter to int
+ * @b : pointer to int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = &a;
+	*a = b;
+	*b = n;
+}
