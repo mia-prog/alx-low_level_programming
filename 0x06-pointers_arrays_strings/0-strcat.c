53cc83b23b2a4bfd70b6037ae4ea19126bb48a35
mia-prog <mechtoubmeriem@gmail.com>
2023-07-26 10:45:59 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,27 @@
 
 /**
  * *_strcat - function commute srtings
- * @dest: param pointer to char
- * @src: param pointer to char
- * Return: value of dest
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (dest[j] != '\0')
-		j++;
+	i = 0;
+	j = 0;
 
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
-		dest[j] = src[i];
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
