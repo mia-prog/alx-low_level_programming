3a2a26e557a359dcfa2b6e6714e2ef9f5a5628d7
meriem <mechtoubmeriem@gmail.com>
2023-08-04 09:57:07 -0700

    task1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-for file in *.c; do gcc -o "${file%.c}" "$file"; done
+
+# Get a list of all .c files in the current directory
+c_files=$(ls *.c 2>/dev/null)
+
+if [ -z "$c_files" ]; then
+  echo "No .c files found in the current directory."
+  exit 1
+fi
+
+# Compile each .c file into .o object files
+for file in $c_files; do
+  gcc -c "$file"
+done
+
+# Create the static library liball.a from the object files
 ar rcs liball.a *.o
+
+# Clean up the object files
+rm -f *.o
+
+echo "Static library liball.a created successfully."
