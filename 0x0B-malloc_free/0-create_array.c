eceecd4a558ce2ca2669b0e96212cace22354c86
meriem <mechtoubmeriem@gmail.com>
2023-08-07 05:11:38 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,15 @@
 char *create_array(unsigned int size, char c)
 {
 	int i;
-	char c[size];
+	char array;
 
-	if(size = 0)
+	if(size == 0)
 		return NULL;
 	else
 	{
 		for(i = 0; i < size; i++)
 		{
-			c[i] = c;
+			char[i] = c;
 		}
 		return(c);
 	}
