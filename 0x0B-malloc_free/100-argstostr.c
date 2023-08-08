93c93a59fbf9c82a2fdcc380bc2f9d03418b4657
meriem <mechtoubmeriem@gmail.com>
2023-08-08 05:07:51 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * argstostr - concatenates all the arguments of a program.
@@ -23,14 +24,14 @@ char *argstostr(int ac, char **av)
 		length += strlen(av[i]) + 1;
 
 	str = malloc(length * sizeof(char));
-	if (str = NULL)
+	if (str == NULL)
 	{
 		free(str);
 		return NULL;
 	}
 
 	current_position = 0;
-	for (int i = 0; i < ac; i++) 
+	for (i = 0; i < ac; i++) 
 	{
 		strcpy(str + current_position, av[i]);
 		current_position += strlen(av[i]);
