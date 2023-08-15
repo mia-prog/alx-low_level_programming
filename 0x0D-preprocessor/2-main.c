356bb8d79e5aa8f38a1ac8a7394404cd2d0b0180
meriem <mechtoubmeriem@gmail.com>
2023-08-15 04:27:31 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+       	printf("This message is from file: %s\n", __FILE__);
+	return 0;
+}
