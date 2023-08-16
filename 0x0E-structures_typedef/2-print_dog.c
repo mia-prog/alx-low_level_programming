c90e4d186cb69e379d37af3cead04f81fbc8eb61
meriem <mechtoubmeriem@gmail.com>
2023-08-16 04:09:22 -0700

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog : print d elements
+ * @d : struct dog
+ * returns : dhkjd
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+			printf("Name : nil\n");
+		else
+			printf("Name : %s\n", d->name);
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
