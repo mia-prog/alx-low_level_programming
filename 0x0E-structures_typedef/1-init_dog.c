55b7f14d29824a0dbe5d72c5893071e03fbdc0a6
meriem <mechtoubmeriem@gmail.com>
2023-08-16 03:40:45 -0700

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,9 +6,13 @@
  * @age : the age of dog
  * @owner : the name of the owner
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
