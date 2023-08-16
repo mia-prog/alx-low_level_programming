7f5b0e7d5fbb8b008cc906f6a52e28b279b34691
meriem <mechtoubmeriem@gmail.com>
2023-08-16 03:37:23 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog : initialize a variable of type struct dog
+ * @d : new instance of type dog
+ * @name : name of dog
+ * @age : the age of dog
+ * @owner : the name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
