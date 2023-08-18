2566e1de4efef1aba555a00f74bb7ef635f9a855
meriem <mechtoubmeriem@gmail.com>
2023-08-18 04:23:02 -0700

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
  *
  * Return: sum of its parameters.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
